15628e88b29d6c524e25627a4ebbdfe96a784b18
Thomas Mayrhofer <thomas.mayrhofer@sclable.com>
2020-11-24 19:02:45 +0100

    failing async test

--- src/index.spec.js
+++ src/index.spec.js
@@ -8,17 +8,33 @@ class SclablePromise {
 
   resolve(value) {
     this.value = value
+    if (this.thenCallback) {
+      this.thenCallback(value)
+    }
+    
   }
 
   then(thenCallback) {
-    thenCallback(this.value)
-    return this
-    // this.thenCallback = thenCallback
-    // TODO: tests are chainable
+    this.thenCallback = thenCallback
+    return new SclablePromise((resolve) => resolve(thenCallback(this.value)))
   }
 }
 
 it('WHEN promise resolves, returns value', () => {
   return promiseThat(new SclablePromise((resolve) => resolve('test')), 
     isFulfilledWith('test'))
+})
+
+it('WHEN promise are async (using setTimeout), returns value', () => {
+  return promiseThat(new SclablePromise((resolve) => setTimeout(() => resolve('test')), 0), 
+    isFulfilledWith('test'))
+})
+
+it('WHEN multiple listeners, returns passes value to all of them', () => {
+  const promise = new SclablePromise((resolve) => resolve('test'))
+  
+  return promiseThat(Promise.all([
+    promise,
+    promise.then((value) => value.toUpperCase())
+  ]), isFulfilledWith(['test', 'TEST']))
 })
